8aa750cbaaffab7d508a96df004bbd7f7663259e
Koala <koala_lord19@yahoo.com>
2026-03-18 16:43:00 -0500

    testing multiplayer chat

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import ItemsOnDeathView from './components/views/overlays/ItemsOnDeathView';
 import PriceCheckerView from './components/views/overlays/PriceCheckerView';
 import DungeonMapView from './components/views/DungeonMapView';
 import GuidedTutorialOverlay from './components/common/GuidedTutorialOverlay';
+import { Chat } from './components/Chat';
 import Game from './components/game/Game';
 import TechDemoGame from './prototyping/TechDemoGame';
 import SaveSlotScreen from './components/screens/SaveSlotScreen';
@@ -256,6 +257,7 @@ const App: React.FC = () => {
                             onImportGame={handleImport}
                             onResetGame={handleReset}
                         />
+                        <Chat username={activeGameState.username} />
                     </div>
                 );
         }
