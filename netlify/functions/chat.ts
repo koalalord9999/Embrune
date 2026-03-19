2ea65504498ddb6cc5d32bf74437673aa8b57038
Koala <koala_lord19@yahoo.com>
2026-03-19 06:00:59 -0500

    Update chat.ts

--- netlify/functions/chat.ts
+++ netlify/functions/chat.ts
@@ -15,7 +15,7 @@ const filter = {
 };
 
 const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID || '',
+  appId: process.env.PUSHER_APPID || '',
   key: process.env.PUSHER_KEY || '',
   secret: process.env.PUSHER_SECRET || '',
   cluster: process.env.PUSHER_CLUSTER || '',
