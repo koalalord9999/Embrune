72751a527c858efe2a1de32a63a7a6ca12082c0b
Koala <koala_lord19@yahoo.com>
2026-03-19 05:52:53 -0500

    Update chat.ts

--- netlify/functions/chat.ts
+++ netlify/functions/chat.ts
@@ -1,7 +1,7 @@
 import { Handler } from '@netlify/functions';
 import Pusher from 'pusher';
-import * as Filter from 'bad-words';
-const filter = new (Filter as any).default();
+const Filter = require('bad-words');
+const filter = new (Filter.default || Filter)();
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID || '',
